eed56202d34f75dee3f2c4c56c5e9bbb26660d8f
Rahul Vishwakarma <119070053+manzil-infinity180@users.noreply.github.com>
2023-05-13 23:38:21 +0530

    Add files via upload
    File Uploaded

---
+++ app.js
@@ -0,0 +1,83 @@
+// function myFunction(x) {
+//   if (x.matches) {
+//     // If media query matches
+//     //   document.body.style.backgroundColor = "yellow";
+//   } else {
+//     //  document.body.style.backgroundColor = "pink";
+//   }
+// }
+
+// var x = window.matchMedia("(max-width: 700px)");
+// myFunction(x); // Call listener function at run time
+// x.addListener(myFunction);
+
+// function myFunction() {
+//   var x = document.getElementById("links");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
+// On click ---  new tab
+
+function laptop() {
+  window.open(
+    "https://www.apple.com/in/shop/buy-mac/macbook-pro/14-inch-macbook-pro",
+    "_blank"
+  );
+}
+function robo() {
+  window.open("http://roboticsclub.mmmut.ac.in/rc/", "_blank");
+}
+function phone() {
+  window.open(
+    "https://www.samsung.com/in/smartphones/?product1=sm-f936bzkdinu&product2=sm-f721bzaainu&product3=sm-s908edrginu",
+    "_blank"
+  );
+}
+
+function watch1() {
+  window.open(
+    "https://www.samsung.com/in/watches/galaxy-watch/galaxy-watch5-pro-black-titanium-lte-sm-r925fzkainu/buy/",
+    "_blank"
+  );
+}
+
+function watch2() {
+  window.open("https://www.apple.com/in/apple-watch-se/", "_blank");
+}
+
+function watch3() {
+  window.open(
+    "https://www.amazon.in/Google-Pixel-Watch-Smartwatch-Champagne/dp/B0BDSDCFN4",
+    "_blank"
+  );
+}
+function airpods() {
+  window.open("https://www.apple.com/in/airpods/", "_blank");
+}
+function headphone() {
+  window.open("https://www.beatsbydre.com/products#headphones", "_blank");
+}
+
+function power() {
+  window.open(
+    "https://www.amazon.in/Pocket-10000mAh-Triple-Charging-Delivery/dp/B08MC57J31/ref=sr_1_5?keywords=power+bank&qid=1684000430&sr=8-5",
+    "_blank"
+  );
+}
+
+function linkedin() {
+  window.open(
+    "https://www.linkedin.com/company/robotics-club-mmmut-gorakhpur/?original_referer=https%3A%2F%2Fwww%2Egoogle%2Ecom%2F&originalSubdomain=in",
+    "_blank"
+  );
+}
+function fb() {
+  window.open("https://www.facebook.com/robomania.mmmut/", "_blank");
+}
+function insta() {
+  window.open("https://www.instagram.com/robotics_club_mmmut/?hl=en", "_blank");
+}
